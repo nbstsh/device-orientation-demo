fb0b6185f5f4ebafde906fd530fb09a9b2c14a14
nbstsh <nbstsh@gmail.com>
2019-08-10 00:52:24 +0900

    Add sensitivity input

--- js/index.js
+++ js/index.js
@@ -1,16 +1,36 @@
 const displayEl = document.querySelector('#display');
+const inputEl = document.querySelector('#sensitivity');
 const messageEl = document.querySelector('#message');
 const ballEl = document.querySelector('#ball');
 
-const maxX = displayEl.clientWidth - ball.clientWidth;
-const maxY = displayEl.clientHeight - ball.clientHeight;
+const ROTATION_SENSITIVITY = 0.5;
+let sensitivity = ROTATION_SENSITIVITY;
+
+inputEl.addEventListener('change', e => {
+	sensitivity = Number(e.target.value) / 10;
+});
+
+let maxX, maxY;
+
+const initMaxValues = () => {
+	maxX = displayEl.clientWidth - ball.clientWidth;
+	maxY = displayEl.clientHeight - ball.clientHeight;
+};
+
+initMaxValues();
+window.addEventListener('resize', initMaxValues);
 
 window.addEventListener('deviceorientation', e => {
 	const { absolute, alpha, beta, gamma } = e;
 	const text = JSON.stringify({ absolute, alpha, beta, gamma }, null, 4);
 	messageEl.textContent = text;
 
-	updateIncrement({ x: beta / 10, y: gamma / 10 });
+	// beta's value range from -180 ~ 180
+	// gamma's value range from -90 ~ 90
+	updateIncrement({
+		x: gamma * sensitivity,
+		y: (beta / 2) * sensitivity
+	});
 });
 
 const moveBall = (x, y) => {
